33b5afd2e44f7551eac5a50719b7ae10501a315d
Alief4fei <alief.fadillah202@gmail.com>
2025-05-18 14:49:49 +0700

    nambahin pilihan dinamis

--- tugas_1/pembalik_string.py
+++ tugas_1/pembalik_string.py
@@ -10,6 +10,20 @@ def balik_string(teks):
         hasil += stack.pop()
     return hasil
 
-kalimat = input("Masukkan string yang ingin dibalik: ")
-hasil = balik_string(kalimat)
-print(f"Hasil balik: {hasil}")
+while True:
+    kalimat = input("Masukkan string yang ingin dibalik: ")
+    hasil = balik_string(kalimat)
+    print(f"Hasil balik: {hasil}")
+    
+    while True:
+        print("\nApakah Anda ingin mengulang program? (y), atau selesai (n)")
+        ulang = input().lower()
+        if ulang == 'y':
+            print("Program akan diulang.")
+            break
+        elif ulang == 'n':
+            print("Program selesai.")
+            exit()
+        else:
+            print("Pilihan tidak valid. Silakan masukkan 'y' atau 'n'.")
+        
