72cc1165c7b24cb5d43c7fc8feefc01a78e66ec2
Alief4fei <alief.fadillah202@gmail.com>
2025-05-23 20:52:33 +0700

    ini antrian ye

--- tugas_2/queue.py
+++ tugas_2/queue.py
@@ -1,12 +1,21 @@
 from collections import deque
 
 dq = deque([])
-limit = 5
+
+while True:
+    try:
+        limit = int(input("Masukkan batas maksimal antrian: "))
+        if limit > 0:
+            break
+        else:
+            print("Limit harus lebih dari 0.")
+    except ValueError:
+        print("Masukkan angka yang valid.")
 
 while True:
     print("\nMenu:")
     print("1. Tambah antrian")
-    print("2. Hapus antrian paling depan")
+    print("2. Hapus antrian (pilih nama)")
     print("3. Lihat antrian")
     print("4. Keluar")
     pilihan = input("Pilih menu (1-4): ")
@@ -21,14 +30,25 @@ while True:
 
     elif pilihan == '2':
         if dq:
-            keluar = dq.popleft()
-            print(f"{keluar} telah keluar dari antrian.")
+            print("\nDaftar antrian :")
+            for nama in dq:
+                print(nama)
+            nama_hapus = input("Masukkan nama yang ingin dihapus: ").strip().lower()
+            if dq[0].lower() == nama_hapus:
+                dq.popleft()
+                print(f"{nama_hapus} telah keluar dari antrian.")
+            elif nama_hapus in dq:
+                print(f"Tidak bisa menghapus {nama_hapus}, karena bukan antrian paling depan.")
+            else:
+                print(f"{nama_hapus} tidak ada dalam antrian.")
         else:
             print("Antrian kosong.")
 
     elif pilihan == '3':
         if dq: 
-            print(f"Daftar antrian: {', '.join(dq)}")
+            print("\nDaftar antrian :")
+            for nama in dq:
+                print(nama)
         else:
             print("Antrian kosong.")
 
